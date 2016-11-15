6b18334922cd968658040e92bf1ef3f2f9d0dc22
MaxDistructo <MaxDistructo@users.noreply.github.com>
2016-11-15 14:28:36 -0600

    Add files via upload

---
+++ io/github/maxdistructo/mdml/download/Download.java
@@ -0,0 +1,58 @@
+package io.github.maxdistructo.download;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+
+public class Download{
+	public static void getFromGithub(String dev, String modId, String version, String type, String file){
+  try {
+		URL website = new URL("https://" + dev + "github.io/" + modId + "/" + type + "/" + version + "/" + modId + "-" + version + file);
+		  ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+		  @SuppressWarnings("resource")
+		FileOutputStream fos = new FileOutputStream("/config/" + dev + "/" + modId + "/" + type + "/"  modId + "-" + version + file);
+	fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+} catch (IOException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+	}
+	public static void getTextureFromWeb(String url, String name, String type, String dev, String modId){
+try {
+	URL website = null;
+try {
+	website = new URL(url);
+} catch (MalformedURLException e) {
+	e.printStackTrace();
+	}
+	ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+	@SuppressWarnings("resource")
+	FileOutputStream fos = new FileOutputStream("/assets/" + dev + "/" + modId+"/" + name + type);
+	fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+} catch (IOException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+	}
+	public static void getFromWeb(String url, String name, String type, String dev, String modId){
+	URL website = null;
+	try {
+		website = new URL(url);
+	} catch (MalformedURLException e1) {
+		e1.printStackTrace();
+	}
+  ReadableByteChannel rbc;
+  FileOutputStream fos;
+try {
+	rbc = Channels.newChannel(website.openStream());
+	  fos = new FileOutputStream("/files/" + dev + "/" + modId+"/" + name + type);
+	  fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+} catch (IOException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+
+	}
+}
---
+++ io/github/maxdistructo/mdml/download/getFiles.java
@@ -0,0 +1,47 @@
+package io.github.maxdistructo.download;
+
+import io.github.maxdistructo.Main;
+import io.github.maxdistructo.reader.*;
+import io.github.maxdistructo.update.*;
+import java.io.*;
+
+public class getFiles{
+  public static void init(){
+    int stor = 0;
+    boolean check;
+    if(new File("/config/maxdistructo/mdml/dev/0.1/").exists(){
+      Download.getFromGithub("maxdistructo", "mdml", "0.1", "dev" , ".cfg");
+    }
+    else{
+      new File("~/config/maxdistructo/mdml/dev/0.1/").mkdirs();
+        Download.getFromGithub("maxdistructo","mdml","0.1","dev", ".cfg");
+    }
+ 
+    ReaderArray.ReaderArray("/config/maxdistructo/mdml/dev/0.1/mdml-0.1.cfg", 1, Main.modIDs);
+    ReaderArray.ReaderArray("/config/maxdistructo/mdml/dev/0.1/mdml-0.1.cfg", 2, Main.devs);
+    ReaderArray.ReaderArray("/config/maxdistructo/mdml/dev/0.1/mdml-0.1.cfg", 3, Main.versions);
+    ReaderArray.ReaderArray("/config/maxdistructo/mdml/dev/0.1/mdml-0.1.cfg", 4, Main.types);
+    ReaderArray.ReaderArray("/config/maxdistructo/mdml/dev/0.1/mdml-0.1.cfg", 5, Main.files);
+    updateCheck.checkSpecific("mdml", "maxdistructo", "dev", ".cfg", check);
+    
+    if(check){
+      System.out.println("HEY! New version of MDML is avaiable at https://maxdistructo.github.com/mdml/dev/versions.html");
+    }
+    else(){
+      System.out.println("MDML is Up To Date.");
+    }
+    
+while(!Main.modIDs[stor].equals(null)){
+  if(new File("~/config/" + Main.devs[stor] + "/" + Main.modIDs[stor] + "/" + Main.types[stor] + "/" + Main.versions[stor]).exists()){
+    Download.getFromGithub(Main.devs[stor],Main.modIDs[stor], Main.versions[stor],Main.types[stor],Main.files[stor]);
+    
+  }
+  else{
+    new File("~/config/" + Main.devs[stor] + "/" + Main.modIDs[stor] + "/" + Main.types[stor] + "/" + Main.versions[stor]).mkdirs();
+    Download.getFromGithub(Main.devs[stor],Main.modIDs[stor], Main.versions[stor],Main.types[stor],Main.files[stor]);
+  }
+    stor++;
+}
+
+  }
+}
